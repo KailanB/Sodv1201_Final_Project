bbea93ccba8f7f34b84c0d8637fc003aa94d4c20
KailanB <kailanbates@gmail.com>
2024-07-21 12:35:50 -0600

    added city, province and role to user account creation
    There's a couple ways we could go about defining a user role. One is a literal string saying Owner or Renter. However it may be easier using 2 bools, one for each owner renter field within the user object. That way you can just write if (owner) etc. Which may be better. Let me know

--- js/createAccountScript.js
+++ js/createAccountScript.js
@@ -1,12 +1,22 @@
 document.addEventListener('DOMContentLoaded', function () {
 
-
-    function User(firstName, lastName, email, phone)
+    // commented arguments are an alternative way of defining roles
+    function User(firstName, lastName, email, phone, city, province, role /* owner, renter*/)
     {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.phoneNumber = phone;
+        this.city = city;
+        this.province = province;
+        this.role = role;
+
+        /*
+        // we could use booleans in stead which may make it easier for checks that way you can simply write
+        // if (user.owner) for if statements, otherwise you would have to write if (user.role === "Owner") which seems tedious
+        this.owner = owner;
+        this.renter = renter;
+        */
 
     }
 
@@ -21,9 +31,28 @@ document.addEventListener('DOMContentLoaded', function () {
         let lastName = document.getElementById("userLastNameInput").value;
         let email = document.getElementById("userEmailAddressInput").value;
         let phoneNumber = document.getElementById("userPhoneInput").value;
-        event.preventDefault();
+        let city = document.getElementById("userCityInput").value;
+        let province = document.getElementById("userProvince").value;
+        let role = document.querySelector('input[name="ownerRenter"]:checked').value;
+
+        /*
+        //using bools method.
+        let owner;
+        let renter;
+        if (role === "Owner")
+        {
+            owner = true;
+            renter = false;
+        }
+        else 
+        {
+            owner = false;
+            renter = true;
+        }
+        let newUser = new User(firstName, lastName, email, phoneNumber, owner, renter);
+        */
         
-        let newUser = new User(firstName, lastName, email, phoneNumber);
+        let newUser = new User(firstName, lastName, email, phoneNumber, city, province, role);
 
         const users = JSON.parse(localStorage.getItem('users')) || [];
         
@@ -37,9 +66,13 @@ document.addEventListener('DOMContentLoaded', function () {
         document.getElementById("userLastNameInput").value = "";
         document.getElementById("userEmailAddressInput").value = "";
         document.getElementById("userPhoneInput").value = "";
+        document.getElementById("userCityInput").value = "";
+        document.getElementById("userProvince").value = "";
+        document.querySelector('input[name="ownerRenter"]:checked').checked = false;
 
-
+        
         // temporary output to test and view user data
+        document.getElementById("forTesting").innerHTML = "";
         users.forEach(user => {
 
             
@@ -47,7 +80,7 @@ document.addEventListener('DOMContentLoaded', function () {
             
             document.getElementById("forTesting").appendChild(newp);
             
-            newp.innerHTML = ` ${user.firstName} ${user.lastName}`;
+            newp.innerHTML = ` ${user.firstName} ${user.lastName} Role: ${user.role}, lives in: ${user.city}`;
             
         })
         // above used for testing
