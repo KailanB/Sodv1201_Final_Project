0b347d83bc65ae82803cf13206cb7a831c969d1c
KailanB <kailanbates@gmail.com>
2024-07-26 16:51:17 -0600

    added a common.js for cookies
    function to set 2 user cookies, it takes 2 parameters, it also sets an expiration date of 5 hours (easily adjustable) to each cookie.
    
    function to return a cookie value. Parameter should be the cookie key, in this case they are written "userEmail" and "userId" value returning is Email or userId as a string.
    
    function checkCookie is not finished. I was using W3schools for this.
    
    Linked the website for citation and reference purposes

---
+++ js/common.js
@@ -0,0 +1,43 @@
+
+
+// COOKIEEEES https://www.w3schools.com/js/js_cookies.asp
+function setUserCookie(userEmail, userId)
+{
+    let days = 1; // update these variables to set cookie expiration
+    let hours = 5;
+    const expireDate = new Date();
+    expireDate.setTime(expireDate.getTime() + (days*hours*60*60*1000));
+    document.cookie = "userEmail=" + userEmail + ";" + expireDate + ";path=/";
+    document.cookie = "userId=" + userId + ";" + expireDate + ";path=/";
+}
+
+function getUserCookie(cname)
+{
+    
+    let name = cname + "=";
+    
+    let decodedCookie = decodeURIComponent(document.cookie);
+    let cookieValues = decodedCookie.split(";");
+    for (let i = 0; i < cookieValues.length ; i++)
+    {
+        
+        let c = cookieValues[i];
+        while (c.charAt(0) == ' ')
+        {
+            c = c.substring(1);
+            
+        }
+        if (c.indexOf(name) == 0)
+        {
+            
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function checkCookie()
+{
+    let userName = getUserCookie("userEmail");
+    let userId = getCookie("userId");
+}
