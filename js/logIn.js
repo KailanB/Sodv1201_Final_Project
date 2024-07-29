71ef57ad784f50f117633f2f576a0a5fd24d803a
KailanB <kailanbates@gmail.com>
2024-07-28 22:51:14 -0600

    Changed method to a better one
    this creates a cookie upon log in to be able to track a specific logged in user

--- js/logIn.js
+++ js/logIn.js
@@ -13,23 +13,36 @@ document.addEventListener('DOMContentLoaded', function () {
         let userEmail = document.getElementById("userEmailLogIn").value;
 
         if(userEmail)
-        {
+        {    
             
-            const users = JSON.parse(localStorage.getItem('users'));
             //forEach iterates fully here before changing locations
-            users.forEach(user => {
-                if(user.email === userEmail)
-                {
+            // users.forEach(user => {
+            //     if(user.email === userEmail)
+            //     {
                     
-                    setUserCookie(userEmail, user.userID); 
-                    // alert(document.cookie);
-                    window.location.href="../home.html";
-                    
-                }
-               
+            //         setUserCookie(userEmail, user.userID); 
+            //         // alert(document.cookie);
+            //         window.location.href="../home.html";   
+            //     }
 
-            });
+            // });
             
+            // better method. Needs further testing for multiple users. Perhaps user dummy data
+            const users = JSON.parse(localStorage.getItem('users'));
+
+            let currentUser = users.find(user => user.email === userEmail);
+            if(currentUser)
+            {
+                setUserCookie(userEmail, currentUser.userID); 
+                window.location.href="../home.html";
+            }
+            else
+            {
+                document.getElementById("logInFailedOutput").innerHTML = `Sorry, user not found. Click <a href="createAccount.html">here</a> to create an account`;
+            }
+             
+
+            // alert(document.cookie);
         }
     }
 
