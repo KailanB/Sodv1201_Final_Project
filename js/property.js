0ce7647ad0ab477445131df79c860bbb8b5f748b
KailanB <kailanbates@gmail.com>
2024-07-28 22:54:54 -0600

    Added dummy data and display only for current user
    ALL PROPERTIES WILL NO LONGER LIST HERE! Only properties that have the same userId as the current user browsing (tracked via cookies) will be displayed here.
    
    Added a property constructor, used with dummy data.
    
    Removed my auto fill fields testing method.
    
    Switched a few owerId variables to userId for clarity purposes.

--- js/property.js
+++ js/property.js
@@ -1,38 +1,103 @@
 document.addEventListener('DOMContentLoaded', function () {
+    
     // Function to load properties from local storage and display them
     function loadProperties() {
-        const properties = JSON.parse(localStorage.getItem('properties')) || [];
+       
+        let properties = JSON.parse(localStorage.getItem('properties')) || [];
+
+        // localStorage.clear();
+        // DUMMY DATA FOR TESTING **************************************** 
+        if(!properties.length) // check if array is empty
+        {
+            
+            let propertiesArray = [
+
+                ["Galleria", "561 4th street", "Calgary", "AB", "Art Gallery", 500, 50, true, true, 300, "Daily", true, 1],
+                ["Sweet Sounds", "1212 5th ave", "Edmonton", "AB", "Recording Studio", 250, 5, true, false, 400, "Weekly", true, 1],
+                ["Theatrics Theatre", "781 1st street", "Saskatoon", "SK", "Theatre", 1000, 100, true, true, 5000, "Monthly", false, 2],
+                ["Creative Center", "22 50th street", "Calgary", "AB", "Painting Rooms", 700, 30, false, true, 1000, "Weekly", true, 2],
+                ["Simple Solutions Art", "820 2nd ave", "Vulcan", "AB", "All Purpose Art Space", 350, 8, true, false, 200, "Daily", true, 3],
+                ["Music Mania", "31 9th ave", "Calgary", "AB", "Music Studio", 850, 15, false, true, 3000, "Monthly", true, 4],
+                ["Sally Studio", "44 Oak Street", "Vancouver", "BC", "Art Gallery", 600, 25, true, false, 200, "Hourly", true, 5],
+                ["Dramatic Theatres ", "87 Elm ave", "Calgary", "AB", "Theatre", 1200, 300, true, true, 8000, "Monthly", true, 1]
+
+
+            ];
+            
+            propertiesArray.forEach( property => 
+            {
+                let newProperty = new Property(property[0], property[1], property[2], property[3], property[4], property[5], property[6], property[7], property[8], property[9], property[10], property[11], property[12]);
+                properties.push(newProperty);
+                
+            });
+            
+            localStorage.setItem('properties', JSON.stringify(properties));
+        }  
+        
+
+
+        properties = JSON.parse(localStorage.getItem('properties')) || [];
         const propertiesContainer = document.getElementById('properties');
         propertiesContainer.innerHTML = '';
-
+        
         properties.forEach(property => {
-            const propertyDiv = document.createElement('div');
-            propertyDiv.className = 'innerPageContent3 dynamicallyCreatedDiv';
-            propertyDiv.innerHTML = `
-                <h2>${property.name}</h2>
-                <p>${property.address}</p>
-                <p>${property.city}, ${property.province}</p>
-                <p>${property.area} sq Meters, Max Occupants: ${property.maxOccupants}</p>
-                <p>Parking: ${property.parking ? 'Yes' : 'No'}</p>
-                <p>Public Transport: ${property.transport ? 'Yes' : 'No'}</p>
-                <div>
-                    <h3>${property.type} (type)</h3>
-                </div>
-                <div>
-                    <p>$${property.price.toFixed(2)} - ${property.rentalTerm}</p>
-                    <p>${property.available ? 'Available Now' : 'Not Available'}</p>
-                </div>
-            `;
-            propertiesContainer.appendChild(propertyDiv);
+            
+            // alert("Cookie user ID: " + getUserCookie("userId") + " property userId: " + property.userId + " Property name: " + property.name);
+
+            // check to display only properties that match current userId
+            if(property.userId === parseInt(getUserCookie("userId")))
+            {
+                const propertyDiv = document.createElement('div');
+                propertyDiv.className = 'innerPageContent3 dynamicallyCreatedDiv';
+                
+                propertyDiv.innerHTML = `
+                    <h2>${property.name}</h2>
+                    <p>${property.address}</p>
+                    <p>${property.city}, ${property.province}</p>
+                    
+                    <p>${property.area} sq Meters, Max Occupants: ${property.maxOccupants}</p>
+                    <p>Parking: ${property.parking ? 'Yes' : 'No'}</p>
+                    <p>Public Transport: ${property.transport ? 'Yes' : 'No'}</p>
+                    <div>
+                        <h3>${property.type} (type)</h3>
+                    </div>
+                    <div>
+                        <p>$${property.price.toFixed(2)} - ${property.rentalTerm}</p>
+                        <p>${property.available ? 'Available Now' : 'Not Available'}</p>
+                    </div>
+                `;
+                
+                propertiesContainer.appendChild(propertyDiv);
+            }
+            
+            
         });
+        
     }
 
+
+    function Property(name, address, city, province, type, area, maxOccupants, parking, transport, price, rentalTerm, available, userId) 
+    {
+        this.name = name;
+        this.address = address;
+        this.city = city;
+        this.province = province;
+        this.type = type;
+        this.area = area;
+        this.maxOccupants = maxOccupants;
+        this.parking = parking;
+        this.transport = transport;
+        this.price = price;
+        this.rentalTerm = rentalTerm;
+        this.available = available;
+        this.userId = userId;
+    }
     // Load properties on page load
     loadProperties();
 
     // Show add property form
     document.getElementById('openAddPropertyDiv').addEventListener('click', function () {
-        document.getElementById('addNewPropertyOuterDiv').style.display = 'flex'; // changed this from block to flex, so that the add property div is center aligned again
+        document.getElementById('addNewPropertyOuterDiv').style.display = 'flex'; // changed this from block to flex, so that the add property div is center aligned again  
     });
 
     // Hide add property form
@@ -56,31 +121,11 @@ document.addEventListener('DOMContentLoaded', function () {
         // above are clear property inputs added
     });
 
-    // Add new property
-    // FOR TESTING ONLY ***********************
-    document.getElementById('propertyNameInput').value = "Sally Studio";
-    document.getElementById('propertyAddressInput').value = "123 Main Street";
-    document.getElementById('propertyCityInput').value = "Calgary";
-    document.getElementById('propertyProvinces').value = "AB";
-    document.getElementById('propertyTypeInput').value = "Recording Studio";
-    document.getElementById('propertAreaInput').value = "200";
-    document.getElementById('propertyMaxOccupancyInput').value = "10";
-    document.getElementById('propertyPriceInput').value = "300.00";
-    document.getElementById('rentalTermSelect').value = "Daily";
-
-
-
-    // END FOR TESTING ****************
-
-
-
-
-
     document.getElementById('addNewPropertyButton').addEventListener('click', function () {
 
         // for testing and edits!
         // localStorage.clear();
-
+        
         const name = document.getElementById('propertyNameInput').value;
         const address = document.getElementById('propertyAddressInput').value;
         const city = document.getElementById('propertyCityInput').value;
@@ -95,7 +140,7 @@ document.addEventListener('DOMContentLoaded', function () {
         const available = document.querySelector('input[name="availability"]:checked').value === 'yes';
 
 
-        const ownerId = parseInt(getUserCookie("userId"));
+        const userId = parseInt(getUserCookie("userId"));
 
         if (name && address && city && province && type && !isNaN(area) && !isNaN(maxOccupants) && !isNaN(price) && rentalTerm) {
             const newProperty = {
@@ -111,7 +156,7 @@ document.addEventListener('DOMContentLoaded', function () {
                 price,
                 rentalTerm,
                 available,
-                ownerId,
+                userId,
             };
 
             // Retrieve existing properties
