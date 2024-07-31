5f0b157bed579aac4551f1876fd8b1b69763b3a4
Deepanshi <155594353+21Deepanshi@users.noreply.github.com>
2024-07-31 14:44:03 -0600

    Add files via upload

--- js/common.js
+++ js/common.js
@@ -1,14 +1,11 @@
-
-
 // COOKIEEEES https://www.w3schools.com/js/js_cookies.asp
 function setUserCookie(userEmail, userId)
 {
 
     let days = 1; // update these variables to set cookie expiration
-    let hours = 5;
-    const expireDate = new Date();
-    
-    expireDate.setTime(expireDate.getTime() + (days*hours*60*60*1000));
+    // let hours = 5;
+    const expireDate = new Date(); 
+    expireDate.setTime(expireDate.getTime() + (days * 24 * 60 * 60 * 1000));
     document.cookie = "userEmail=" + userEmail + ";" + expireDate + ";path=/";
     document.cookie = "userId=" + userId + ";" + expireDate + ";path=/";
 
@@ -25,12 +22,12 @@ function getUserCookie(cname)
     {
         
         let c = cookieValues[i];
-        while (c.charAt(0) == ' ')
+        while (c.charAt(0) === ' ')
         {
             c = c.substring(1);
             
         }
-        if (c.indexOf(name) == 0)
+        if (c.indexOf(name) === 0)
         {
             
             return c.substring(name.length, c.length);
