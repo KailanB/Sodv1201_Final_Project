f6aa7562c3f6f56f301af6ad84cfe4fe4ea0fcfb
KailanB <kailanbates@gmail.com>
2024-08-05 13:31:12 -0600

    Added a view property page for single property
    When a user clicks on a property div in the home page it leads to this html page + userId and propertyId added to the url.
    
    The script then has a self invoking function that gathers both Id's from the url then uses them to find the according user and property from the local files. After finding the correct user and property it displays their data on the page.
    
    This page will likely have a lot of issues working unless you create a user in your local storage so that an appropriate user is connected to the ID.
    
    I will likely create dummy "user" data to pull from as well so that this function works always.
    
    perhaps adding some if / else statements on the variable created using the .find method might be a good idea. This will make sure that the entire page does not crash if there was an issue retrieving the data

---
+++ js/singlePropertyView.js
@@ -0,0 +1,91 @@
+document.addEventListener('DOMContentLoaded', function () {  
+
+    
+
+    function getIdFromUrl(IdName)
+    {
+        let name = IdName + "=";
+        let url = window.location;
+        url = url.toString();
+        
+        // find location in string of the search parameter
+        let positionStart = url.indexOf(name);
+        if(positionStart === -1)
+        {
+            return null;
+        }
+        else
+        {
+
+            // find the first question mark after the search parameter is found
+            let positionEnd = (url.substring(positionStart)).indexOf("?") + positionStart;
+
+            // slice string section containing ID of parameter
+            return url.slice(positionEnd-1, positionEnd);
+        }
+        
+    
+    }
+
+    let propertyId = parseInt(getIdFromUrl("propertyId"));
+    let userId = parseInt(getIdFromUrl("userId"));
+    const propertyDisplay = document.getElementById("propertyDisplay");
+    const propertyNameTitle = document.getElementById("propertyNameTitle");
+    const propertyNameHeader = document.getElementById("propertyNameHeader");
+
+
+    const localUsers = JSON.parse(localStorage.getItem('users')) || [];
+    const localProperties = JSON.parse(localStorage.getItem('properties')) || [];
+    
+    let property = localProperties.find((property) => property.propertyId === propertyId && property.userId === userId);
+
+
+    // alert("UserId: " + userId + "propertyId: " + propertyId);
+    // alert(property);
+    // alert(property.name);
+
+    let owner = localUsers.find((user) => user.userId === userId);
+
+
+
+    // alert("My cookie: " + getUserCookie("userId") + " owner is: " + owner);
+    // alert("property userId: " + property.userId);
+    
+    
+    const propertyDiv = document.createElement('div');
+    propertyDiv.classList.add('innerPageContent3', 'dynamicallyCreatedDiv');
+    propertyDiv.innerHTML = `
+        <h2>${property.name}</h2>
+        <p>${property.address}</p>
+        <p>${property.city} ${property.province}</p>
+        <p>${property.area} sq Meters, Max Occupants: ${property.capacity}</p>
+        <p>Parking: ${property.parking ? 'Yes' : 'No'}</p>
+        <p>Public Transport: ${property.publicTransport ? 'Yes' : 'No'}</p>
+        <div>
+            <h3>${property.type}</h3>
+        </div>
+        <div>
+            <p>$${property.price} - ${property.rentalTerm}</p>
+            <p>${property.availability ? 'Available Now' : 'Not Available'}</p>
+        </div>
+    `;
+    
+    const ownerDiv = document.createElement('div');
+    ownerDiv.classList.add('innerPageContent3', 'dynamicallyCreatedDiv');
+    
+    ownerDiv.innerHTML = `
+    <h2>${owner.firstName} ${owner.lastName}</h2>
+    <p>Email: ${owner.email}</p>
+    <p>Phone Number: ${owner.phoneNumber}</p>    
+    `;
+    
+    propertyDisplay.appendChild(propertyDiv);
+    propertyDisplay.appendChild(ownerDiv);
+
+    propertyNameTitle.innerHTML = property.name + " Property Page";
+    propertyNameHeader.innerHTML = property.name + " Property Page";
+    // alert("prop id" + propertyId + " user Id: " + userId);
+
+
+
+});
