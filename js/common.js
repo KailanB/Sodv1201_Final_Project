e081a2ece929f3553d3198ce70c7cd0fa90fad3f
Deepanshi <155594353+21Deepanshi@users.noreply.github.com>
2024-08-06 11:54:41 -0600

    Add files via upload
    Updated common.js where you can see the log out display, a clear cookie, also the cookie is clear by userEmail no need to include the userId so it may cause issue with login and logout button. And the lines those are updated I right down the comments

--- js/common.js
+++ js/common.js
@@ -40,11 +40,52 @@ function getUserCookie(cname)
     return "";
 }
 
-function checkCookie()
-{
-    let userName = getUserCookie("userEmail");
-    let userId = getCookie("userId");
+// function checkCookie()
+// {
+//     let userName = getUserCookie("userEmail");
+//     let userId = getCookie("userId");
+// }
+
+
+//check the cookie and manage the display of login and logout
+function checkUserLoggedIn() {
+    let userEmail = getUserCookie("userEmail");
+    // let userId = getCookie("userId");
+    let loginButtonContainer = document.getElementById("loginButtonContainer");
+    let logoutButtonContainer = document.getElementById("logoutButtonContainer");
+
+    if (userEmail) {
+        loginButtonContainer.style.display = "none";
+        logoutButtonContainer.style.display = "block";
+    } else {
+        loginButtonContainer.style.display = "block";
+        logoutButtonContainer.style.display = "none";
+    }
 }
+// it will clear the cookie after I click on the log out button
+function clearCookies() {
+    const futureDate = new Date();
+    futureDate.setFullYear(futureDate.getFullYear() + 1); // Set expiration to 1 year from now.
+    const expires = "expires=" + futureDate.toUTCString(); //is used to create a string representation of the expiration date for a cookie in the correct format
+    //document.cookie: returns a string containing all cookies, separated by semicolons. and split(";") divides this string into an array.
+    //c.split("=")[0] splits the cookie string at the = sign and takes the first part, which is the cookie name.
+    //expires + "; path=/" appends the expires attribute to set the expiration date, and path=/ ensures the cookie is valid for the entire domain
+    document.cookie.split(";").forEach(function(c) {
+        let cookieName = c.trim().split("=")[0];
+        // Set the cookie with the same name, but with an updated expiration date
+        document.cookie = cookieName + "=; " + expires + "; path=/";
+    });
+}
+
+
+// Event listener for logout button
+document.addEventListener('DOMContentLoaded', function () {
+    checkUserLoggedIn();
+    document.getElementById("logoutButton").addEventListener("click", function() {
+        clearCookies();
+        window.location.href = "pages/logIn.html"; // Redirect to login page after logging out
+    });
+});
 
 // compares user cookie id with the id of all registered users, when the function finds a matching user it returns that user object, otherwise returns null. 
 // this can be used to retrieve other user data based on the currently browsing user
