afdfc40943017ac56f64a3a251171c658dcc7acd
KailanB <kailanbates@gmail.com>
2024-08-15 18:47:09 -0600

    functions to save or retrieve data. Used to not further clutter the routes file
    If you have any lengthy retrieve or save file functions please put them here!

---
+++ dataScripts.js
@@ -0,0 +1,48 @@
+const fs = require('fs');
+
+
+async function saveProperty(property, fileName)
+{
+    const allProperties = await retrieveData(fileName);
+
+    allProperties.push(property);
+
+    fs.writeFileSync(fileName, JSON.stringify(allProperties, null, 2));
+    console.log("write file success");
+
+}
+
+function retrieveData(fileName)
+{
+    return new Promise((res, rej) => {
+
+        fs.readFile(fileName, 'utf8', (error, data) => {
+
+            if(error)
+            {
+                
+                rej(error);
+                return;
+            }
+            else
+            {
+                
+                const dataObject = JSON.parse(data);
+                
+                res(dataObject);
+            }
+        })
+
+
+
+
+    });
+
+
+}
+
+
+
+
+
+module.exports = { saveProperty, retrieveData };
