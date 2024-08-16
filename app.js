673670ffa7f626ecf0321e94dccee3c596962d7a
KailanB <kailanbates@gmail.com>
2024-08-15 18:45:59 -0600

    Added the basic express requires, PORT 3000, start server etc.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 // NPM init, express, and nodemon are all set up. Nothing much else has been added for the time being.
-
 const express = require('express');
 
-const api = require('./api');
-
+const routes = require('./routes.js');
 const app = express();
 const PORT = 3000;
 
+// always have this before the routes
 app.use(express.static('public'));
 
-app.use('/api', api);
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: false}));
+
+app.use('/', routes);
 
 app.listen(PORT, (error) => {
 
