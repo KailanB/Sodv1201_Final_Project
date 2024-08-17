b49df7eae3a30fd71dc12877d2504e1069b2b8ec
KailanB <kailanbates@gmail.com>
2024-08-16 19:22:54 -0600

    Script to display property details on view property page

---
+++ public/js/viewProperty.js
@@ -0,0 +1,83 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+
+    displayProperty();
+    // alert(parseInt(getIdFromUrl("userId")) + " " + parseInt(getIdFromUrl("propertyId")));
+
+    async function displayProperty()
+    {
+
+        const propertyDisplay = document.getElementById("propertyDisplay");
+        const propertyNameTitle = document.getElementById("propertyNameTitle");
+        const propertyNameHeader = document.getElementById("propertyNameHeader");
+
+        let propertyId = parseInt(getIdFromUrl("propertyId"));
+        let userId = parseInt(getIdFromUrl("userId"));
+
+        fetch(`/viewProperty/${propertyId}/${userId}`)
+        .then(response => response.json())
+        .then(property => 
+        {
+
+            // alert(property.userId + property.propertyId);
+            const propertyDiv = document.createElement('div');
+            propertyDiv.classList.add('innerPageContent3', 'dynamicallyCreatedDiv');
+            propertyDiv.innerHTML = `
+            <h2>${property.name}</h2>
+            <p>${property.address}</p>
+            <p>${property.city} ${property.province}</p>
+            <p>${property.area} sq Meters, Max Occupants: ${property.capacity}</p>
+            <p>Parking: ${property.parking ? 'Yes' : 'No'}</p>
+            <p>Public Transport: ${property.publicTransport ? 'Yes' : 'No'}</p>
+            <div>
+                <h3>${property.type}</h3>
+            </div>
+            <div>
+                <p>$${property.price} - ${property.rentalTerm}</p>
+                <p>${property.availability ? 'Available Now' : 'Not Available'}</p>
+            </div>
+        `;
+
+        propertyDisplay.appendChild(propertyDiv);
+
+
+        propertyNameTitle.innerHTML = property.name + " Property Page";
+        propertyNameHeader.innerHTML = property.name + " Property Page";
+
+        })
+
+
+    }
+
+
+
+    function getIdFromUrl(IdName)
+    {
+        let name = IdName + "=";
+        let url = window.location;
+        url = url.toString();
+        
+        // find location in string of the search parameter
+        let positionStart = url.indexOf(name);
+        if(positionStart === -1)
+        {
+            return null;
+        }
+        else
+        {
+
+            // find the first question mark after the search parameter is found
+            let positionEnd = (url.substring(positionStart)).indexOf("&") + positionStart;
+
+            // slice string section containing ID of parameter
+            // slice starts after the search name within url and ends at the question mark
+            return url.slice((positionStart+name.length), positionEnd);
+        }
+        
+    
+    }
+
+
+
+
+});
